4bf4cdd0b001c580cd2c075acabe7b7418cbd233
Fadzaa <fadza20@gmail.com>
2024-05-27 23:33:15 +0700

    add products api

---
+++ database/migrations/2024_05_27_153238_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->text('description');
+            $table->enum('type', ['geprek', 'ricebowl', 'snack', 'minuman'])->default('geprek');
+            $table->decimal('price', 8, 2);
+            $table->decimal('rating', 2, 1)->default(0);
+            $table->unsignedInteger('stock')->default(0);
+            $table->string('image');
+            $table->unsignedInteger('total_rating')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
