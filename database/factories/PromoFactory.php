b118ea9c884d60caed2f592436125b0f5c3a3b5e
Fadzaa <fadza20@gmail.com>
2024-06-04 11:52:05 +0700

    fix promo schema

--- database/factories/PromoFactory.php
+++ database/factories/PromoFactory.php
@@ -22,13 +22,10 @@ class PromoFactory extends Factory
             'qty' => $this->faker->randomNumber(2),
             'start_date' => $this->faker->date(),
             'end_date' => $this->faker->date(),
-//            'promo_code' => $this->faker->unique()->bothify('Promo##??'),
-//            'discount' => $this->faker->randomFloat(2, 1, 100),
-//            'min_purchase' => $this->faker->randomFloat(2, 1, 1000),
-//            'max_discount' => $this->faker->randomFloat(2, 1, 500),
-//            'is_active' => $this->faker->boolean,
-//            'usage_limit' => $this->faker->randomNumber(3),
-//            'used_count' => $this->faker->randomNumber(2),
+            'discount' => $this->faker->randomDigit(),
+            'is_active' => $this->faker->boolean,
+            'usage_limit' => $this->faker->randomNumber(3),
+            'used_count' => $this->faker->randomNumber(2),
             'image' => 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgeNOJGZL4W6MsX-Dn8uOACfuANVfADL6R84Mr2nhLVO7zz4Ir6lnUZe19DO-ZwKue-SH45fnCnYBLQrborsyJx3iQKNvvxKNAeD7KHs5LncMsBUXsNqU0XOAzy4As4rqcef8HGqH4g02QI/s0/geprekbensu_1028hd.jpg',
         ];
     }
