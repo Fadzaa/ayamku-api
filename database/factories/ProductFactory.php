93820caac73955802277686504a070052f54ce44
Fadzaa <fadza20@gmail.com>
2024-08-05 09:38:06 +0700

    add sample seeder

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -16,14 +16,34 @@ class ProductFactory extends Factory
      */
     public function definition(): array
     {
+        $category = $this->faker->randomElement(['geprek', 'ricebowl', 'snack', 'minuman']);
+
+        $categoryData = [
+            'geprek' => [
+                'name' => 'Geprek Sample',
+                'image' => 'https://png.pngtree.com/png-clipart/20230319/original/pngtree-sweet-cheese-geprek-chicken-png-image_8995918.png',
+            ],
+            'ricebowl' => [
+                'name' => 'Ricebowl Sample',
+                'image' => 'https://img.freepik.com/premium-photo/spicy-yummy-chicken-rice-bowl-with-vegetables-isolated-white-background_787273-29390.jpg',
+            ],
+            'snack' => [
+                'name' => 'Snack',
+                'image' => 'https://www.dairyqueen.com/dA/0851e38bb2/chicken_strips_fry_rings.png',
+            ],
+            'minuman' => [
+                'name' => 'Minuman',
+                'image' => 'https://static.vecteezy.com/system/resources/previews/027/254/417/original/delicious-ice-tea-png.png',
+            ],
+        ];
+
+
         return [
-            //unique food name
-            'name' => $this->faker->unique()->word,
+            'name' => $categoryData[$category]['name'],
             'description' => $this->faker->sentence,
-            'price' => $this->faker->randomFloat(2, 1, 100),
-            'category' => $this->faker->randomElement(['geprek', 'ricebowl', 'snack', 'minuman']),
-//            'stock' => $this->faker->numberBetween(0, 100),
-            'image' => 'https://d1vbn70lmn1nqe.cloudfront.net/prod/wp-content/uploads/2023/07/17055245/Rendah-Kalori-Ini-Resep-Ayam-Geprek-Pedas-Ala-Rumahan-.jpg',
+            'price' => $this->faker->numberBetween(10000, 20000),
+            'category' => $category,
+            'image' => $categoryData[$category]['image'],
         ];
     }
 }
